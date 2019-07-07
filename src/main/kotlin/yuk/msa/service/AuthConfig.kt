ed8a99324aa96e88a3a371c26cf84aa5364c31e0
yongsoo <yozx9079@naver.com>
2019-07-08 01:14:18 +0900

    add jwt token config

--- src/main/kotlin/yuk/msa/service/AuthConfig.kt
+++ src/main/kotlin/yuk/msa/service/AuthConfig.kt
@@ -1,12 +1,45 @@
 package yuk.msa.service
 
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore
+import org.springframework.security.oauth2.provider.token.TokenStore
+import org.springframework.security.oauth2.provider.token.DefaultTokenServices
+import org.springframework.context.annotation.Primary
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer
+
+
 
 @Configuration
 class AuthConfig : ResourceServerConfigurerAdapter() {
     override fun configure(http: HttpSecurity) {
         http.authorizeRequests().anyRequest().authenticated()
+    }
+
+    override fun configure(config: ResourceServerSecurityConfigurer) {
+        config.tokenServices(tokenServices())
+    }
+
+    @Bean
+    @Primary
+    fun tokenServices(): DefaultTokenServices {
+        val defaultTokenServices = DefaultTokenServices()
+        defaultTokenServices.setTokenStore(tokenStore())
+        return defaultTokenServices
+    }
+
+    @Bean
+    fun tokenStore(): TokenStore {
+        return JwtTokenStore(accessTokenConverter())
+    }
+
+    @Bean
+    fun accessTokenConverter(): JwtAccessTokenConverter {
+        val converter = JwtAccessTokenConverter()
+        converter.setSigningKey("asdfasdfasdf")
+        return converter
     }
 }
